13755b0b504777db2c1caea528983fb16ce32176
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2024-03-28 23:54:55 +0100

    Player class added

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,9 +1,26 @@
 #pragma once
-#include <string>
+
+#include "Injury.hpp"
+#include "Nationality.hpp"
 
 class Player
 {
+public:
+    Player(std::string new_first_name, std::string new_last_name, int new_overall,
+           const Injury &new_injury,
+           Nationality new_nationality, int new_goals = 0, int new_assists = 0);
+    auto getName() -> std::string;
+    auto getNumberOfGoals() -> int;
+    auto getNumberOfAssists() -> int;
+    auto getNationality() -> Nationality;
+    auto getInjury() -> Injury;
+
 protected:
     std::string first_name;
     std::string last_name;
+    int overall;
+    Injury injury;
+    Nationality nationality;
+    int goals;
+    int assists;
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,38 @@
-#include "../include/Player.hpp"
+#include "Player.hpp"
+
+#include <utility>
+
+Player::Player(std::string new_first_name, std::string new_last_name, int new_overall,
+               const Injury &new_injury,
+               Nationality new_nationality, int new_goals, int new_assists) : first_name{std::move(new_first_name)},
+                                                                              last_name{std::move(new_last_name)},
+                                                                              overall{new_overall},
+                                                                              injury{new_injury},
+                                                                              nationality{new_nationality},
+                                                                              goals{new_goals}, assists{new_assists}
+{}
+
+auto Player::getName() -> std::string
+{
+    return first_name + last_name;
+}
+
+auto Player::getNumberOfGoals() -> int
+{
+    return goals;
+}
+
+auto Player::getNumberOfAssists() -> int
+{
+    return assists;
+}
+
+auto Player::getNationality() -> Nationality
+{
+    return nationality;
+}
+
+auto Player::getInjury() -> Injury
+{
+    return injury;
+}
