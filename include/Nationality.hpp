b2df86f1c21c88aeb9b3f6acef2c2cbef04a0059
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2024-03-28 23:40:44 +0100

    Nationality enum added

---
+++ include/Nationality.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+enum class Nationality
+{
+    Germany,
+    Spain,
+    France,
+    England,
+    Turkey,
+    Albania,
+    Belgium,
+    Hungary,
+    Denmark,
+    Romania,
+    Portugal,
+    Scotland,
+    Netherlands,
+    Italy,
+    Croatia,
+    CzechRepublic,
+    Austria,
+    Serbia,
+    Slovenia,
+    Switzerland,
+    Slovakia,
+    Poland,
+    Ukraine,
+    Georgia
+};
