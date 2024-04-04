4dc049f610fb2f226883764bfe842111ef713d73
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2024-04-04 20:39:39 +0200

    parameter name fixed

--- include/player/Goalkeeper.hpp
+++ include/player/Goalkeeper.hpp
@@ -6,7 +6,7 @@ class Goalkeeper final : public Player
 {
 public:
     Goalkeeper(std::string new_first_name, std::string new_last_name, int new_overall,
-               Nationality new_nationality, int clean_sheets);
+               Nationality new_nationality, int new_clean_sheets);
 
     [[nodiscard]] auto getCleanSheets() const -> int;
 
