93213bb23f6f4ccb6fa8d56a5847ba2334fb91e3
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2024-04-04 20:39:48 +0200

    Defender class added

---
+++ src/player/Defender.cpp
@@ -0,0 +1,15 @@
+#include "player/Defender.hpp"
+
+Defender::Defender(std::string new_first_name, std::string new_last_name, const int new_overall,
+                   const Nationality new_nationality, const int new_clean_sheets) : Player{
+    std::move(new_first_name),
+    std::move(new_last_name), new_overall,
+    new_nationality
+}, clean_sheets{new_clean_sheets}
+{
+}
+
+auto Defender::getCleanSheets() const -> int
+{
+    return clean_sheets;
+}
