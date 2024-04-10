c01151c53780542f5213b1012ca051da20ea6059
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2024-04-10 21:20:21 +0200

    yellow and red cards added

--- include/player/PlayerStatistics.hpp
+++ include/player/PlayerStatistics.hpp
@@ -2,7 +2,9 @@
 
 struct PlayerStatistics
 {
-    int goals;
-    int assists;
-    int clean_sheets;
+    int goals{0};
+    int assists{0};
+    int clean_sheets{0};
+    int yellow_cards{0};
+    int red_cards{0};
 };
